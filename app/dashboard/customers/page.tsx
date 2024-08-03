1c545e5266ddabd79a6d792ee3a4f65787b29fa0
Ochoagram <ricardogochoa@me.com>
2024-08-02 23:54:48 -0600

    fix error

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,11 @@
+import React, { ReactNode } from 'react';
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
